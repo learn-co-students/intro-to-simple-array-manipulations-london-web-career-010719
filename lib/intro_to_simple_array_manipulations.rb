95ece841283e5905a1d100cc983039db27cb2f26
parik panchal <parikspanchal@icloud.com>
2018-12-27 19:57:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,23 @@
 def using_push(array, string)
   array.push(string)
 end
+
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  popped = array.pop()
+  popped
+end
+
+
+def pop_with_args(array)
+end
+
+def using_shift(array)
+  shifted = array.shift
+  shifted
+end
+
